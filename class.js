4daf65c2e77df149edda308ddeb180ad2e1b10d7
sworder <fenxianga123@gmail.com>
2015-08-25 15:00:58 +0800

    浏览器内核判断
    判断各个浏览器内核（包括微信浏览器）

--- class.js
+++ class.js
@@ -35,3 +35,44 @@ function ctrlSelect(elem) {
         });
 	});
 }
+
+/**
+ * js判断各个浏览器内核
+ * @return {[type]} [description]
+ */
+function getExplorer() {
+	var explorer = window.navigator.userAgent ;
+	//ie 
+	if (explorer.indexOf("MSIE") >= 0) {
+	alert("ie");
+	}
+	//firefox 
+	else if (explorer.indexOf("Firefox") >= 0) {
+	alert("Firefox");
+	}
+	//Chrome
+	else if(explorer.indexOf("Chrome") >= 0){
+	alert("Chrome");
+	}
+	//Opera
+	else if(explorer.indexOf("Opera") >= 0){
+	alert("Opera");
+	}
+	//Safari
+	else if(explorer.indexOf("Safari") >= 0){
+	alert("Safari");
+	}
+}
+
+/**
+ * 判断是否为微信浏览器
+ * @return {Boolean} [description]
+ */
+function is_weixin(){
+	var ua = navigator.userAgent.toLowerCase();
+	if(ua.match(/MicroMessenger/i)=="micromessenger") {
+		return true;
+ 	} else {
+		return false;
+	}
+}
